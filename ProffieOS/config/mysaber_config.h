e15e44752b4f4699e5b75b94215c64571beb7054
jacobrvu <88707346+jacobrvu@users.noreply.github.com>
2025-03-01 19:25:24 +0800

    Update mysaber_config.h

--- ProffieOS/config/mysaber_config.h
+++ ProffieOS/config/mysaber_config.h
@@ -2,7 +2,7 @@
 #include "proffieboard_v3_config.h"
 #define NUM_BLADES 1
 #define NUM_BUTTONS 1
-#define VOLUME 1600
+#define VOLUME 3000
 const unsigned int maxLedsPerStrip = 144;
 #define CLASH_THRESHOLD_G 1.0
 #define ENABLE_AUDIO
